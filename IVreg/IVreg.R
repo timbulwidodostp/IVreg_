4360cb25b6e7999a4400982d661c8e46cf23fbfb
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-10-29 20:35:11 +0700

    Use IVreg (ManyIV) With (In) R Software

--- IVreg/IVreg.R
+++ IVreg/IVreg.R
@@ -5,7 +5,7 @@
 install.packages("remotes")
 remotes::install_github("kolesarm/ManyIV")
 library("ManyIV")
-spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/IVreg_/main/IVreg/IVreg.csv",sep = ";")
+IVreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/IVreg_/main/IVreg/IVreg.csv",sep = ";")
 # Estimation Instrumental Variables Regression Use IVreg (ManyIV) With (In) R Software
 IVreg <-  IVreg(lwage~education+black+married | as.factor(qob),data=IVreg, inference=c("standard", "re", "il", "lil"))
 print(IVreg)
