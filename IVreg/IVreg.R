4e50d020f1e28dbbd02cdbbbdc7c153c44cf2233
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-10-29 20:19:42 +0700

    Use IVreg (ManyIV) With (In) R Software

---
+++ IVreg/IVreg.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Instrumental Variables Regression Use IVreg (ManyIV) With (In) R Software
+install.packages("remotes")
+remotes::install_github("kolesarm/ManyIV")
+library("ManyIV")
+spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/IVreg_/main/IVreg/IVreg.csv",sep = ";")
+# Estimation Instrumental Variables Regression Use IVreg (ManyIV) With (In) R Software
+IVreg <-  IVreg(lwage~education+black+married | as.factor(qob),data=IVreg, inference=c("standard", "re", "il", "lil"))
+print(IVreg)
+# Instrumental Variables Regression Use IVreg (ManyIV) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
